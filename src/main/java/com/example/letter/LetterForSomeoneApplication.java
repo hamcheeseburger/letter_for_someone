d27c07a276e2f4267af3b64353651c648d56c5d2
hamcheeseburger <yoo77hyeon@gmail.com>
2021-07-03 19:52:41 +0900

    first

---
+++ src/main/java/com/example/letter/LetterForSomeoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.letter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetterForSomeoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetterForSomeoneApplication.class, args);
+	}
+
+}
